5c3af801a72d082dc54b2c29354bdb983ec1b4c8
1101137154 <mahendraagnidharma@yahoo.co.id>
2015-04-23 17:57:24 +0800

    my programming 2 project commit 1

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix
+## EN: make cacheable matrix from normal matrix
+## ID: buat matriks cacheable dari matrix normal
 
-## Write a short comment describing this function
+## cacheSolve
+## EN: return inverse of matrix, if already have, take the old cache value
+## ID: menghasilkan invers dari matrix, jika sudah ada, ambil nilai yg ter-cache
+
+## ex:
+## B = matrix(c(2, 4, 3, 1), nrow=2,ncol=2)
+## A <- makeCacheMatrix(B)
+## cacheSolve(A)
+
+## assume that the matrix supplied is always invertible
+## take argument of normal matrix, return a cacheable matrix 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setinverse  <- function(inverse) m <<- inverse
+    getinverse <- function() m
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## take argument of cacheable matrix, return inverse result of matrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    m <- x$getinverse()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setinverse(m)
+    m
 }
